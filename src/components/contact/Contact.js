1f821abb04f4e1f608afa553fc07cf28bf070738
alamin-cse44 <alamincse44@gmail.com>
2023-06-22 04:19:50 +0600

    particle is added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -14,11 +14,107 @@ import ContactForm from "./ContactForm";
 
 import contact from "../../images/contact.png";
 
+// import { useCallback } from "react";
+// import Particles from "react-particles";
+// import { loadFull } from "tsparticles";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 import "./Contact.scss";
 
 const Contact = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
   return (
     <Box className="contact" sx={{ mx: 3, mt: 10 }}>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          // background: {
+          //   color: {
+          //     value: "#0d47a1",
+          //   },
+          // },
+          fpsLimit: 50,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 100,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 900,
+              },
+              value: 30,
+            },
+            opacity: {
+              value: 0.1,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      
       <Box sx={{ textAlign: "center" }}>
         <Title1 title="Contact" />
         <Title2 title="Contact With Me" />
@@ -175,7 +271,7 @@ const Contact = () => {
           </Grid>
 
           <Grid item xs={12} md={6} lg={8}>
-           <ContactForm />
+            <ContactForm />
           </Grid>
         </Grid>
       </Box>
